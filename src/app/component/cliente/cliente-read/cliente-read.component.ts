b7f42058a4dd1ac5ce659f44ada82e70117437b5
Gustavimgago <gustavohfsantos2007@gmail.com>
2025-07-09 16:14:20 -0300

    4.9.7

--- src/app/component/cliente/cliente-read/cliente-read.component.ts
+++ src/app/component/cliente/cliente-read/cliente-read.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ClienteService } from '../cliente.service';
 import { Cliente } from './cliente.model';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-cliente-read',
   templateUrl: './cliente-read.component.html',
   styleUrls: ['./cliente-read.component.css']
 })
-export class ClienteReadComponent implements OnInit{
+export class ClienteReadComponent implements OnInit {
 
-  cliente!: Cliente[];
+  cliente!: MatTableDataSource<Cliente>; // Usando MatTableDataSource para facilitar a filtragem
   displayedColumns = [
     'cliId', 
     'cliNome', 
@@ -22,10 +23,15 @@ export class ClienteReadComponent implements OnInit{
   constructor(private clienteService: ClienteService) {}
 
   ngOnInit(): void {
-      this.clienteService.read().subscribe(cliente => {
-        this.cliente = cliente;
-        console.log(cliente)
-      });
+    this.clienteService.read().subscribe(cliente => {
+      this.cliente = new MatTableDataSource(cliente); // Inicializa o MatTableDataSource
+      console.log(cliente);
+    });
   }
 
+  aplicarFiltro(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.cliente.filter = filterValue.trim().toLowerCase(); // Aplica o filtro
+  }
 }
+
